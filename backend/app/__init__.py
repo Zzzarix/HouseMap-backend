f3087c99c8cec9d4f9fd9b3a7101f8b5ca76e0eb
Zzzarix <android1vvv@gmail.com>
2023-07-04 20:24:12 +1200

    fix

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -67,7 +67,7 @@ async def points_upload(map_id, point_id):
         pass
 
     for name, image in images.items():
-        filename = os.path.join(filespath, secure_filename(image.name))
+        filename = os.path.join(filespath, secure_filename(image.filename))
         filenames.append(secure_filename(image.filename))
         image.save(filename)
 
